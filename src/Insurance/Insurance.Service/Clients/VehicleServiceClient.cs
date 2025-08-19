c13ce71cd974b4c1bb5372ed3fb0c82a9ecd4ab0
Rasmus Olsson <rasmusolssondev@gmail.com>
2025-08-19 23:33:51 +0200

    Fix input validation for batch request

--- src/Insurance/Insurance.Service/Clients/VehicleServiceClient.cs
+++ src/Insurance/Insurance.Service/Clients/VehicleServiceClient.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text.Json;
 using Insurance.Service.Extensions;
 
 namespace Insurance.Service.Clients;
@@ -6,13 +7,15 @@ namespace Insurance.Service.Clients;
 public class VehicleServiceClient : IVehicleServiceClient
 {
     private readonly HttpClient _httpClient;
+    private readonly ILogger<VehicleServiceClient> _logger;
 
-    public VehicleServiceClient(HttpClient httpClient, IConfiguration configuration)
+    public VehicleServiceClient(HttpClient httpClient, IConfiguration configuration, ILogger<VehicleServiceClient> logger)
     {
         var baseUrl = configuration.GetValue<string>("VehicleService:BaseUrl")
             ?? throw new InvalidOperationException("VehicleService:BaseUrl is not configured.");
 
         _httpClient = httpClient;
+        _logger = logger;
         _httpClient.BaseAddress = new Uri(baseUrl);
     }
 
@@ -25,11 +28,22 @@ public class VehicleServiceClient : IVehicleServiceClient
 
         var response = await _httpClient.PostAsJsonAsync("/api/v1/vehicles/batch", registrationNumbers);
 
-        if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+        //TODO: Consider adding partial not found identification of batch entries
+        // Potential data discrepancy  
+        if (response.StatusCode == HttpStatusCode.NotFound)
         {
+            _logger.LogWarning("One or more registrations were not found. {registrationNumbers}", JsonSerializer.Serialize(registrationNumbers));
             return [];
         }
 
+        //TODO: Consider adding partial bad request handling of batch entries
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            _logger.LogWarning("One or more registrations were not in the correct format. {registrationNumbers}", JsonSerializer.Serialize(registrationNumbers));
+            return [];
+        }
+
+        //TODO: Consider fallback strategy if service is down.
         response.EnsureSuccessStatusCode();
 
         var vehicles = await response.Content.ReadFromJsonAsync<Vehicle.Service.Contracts.Vehicle[]>();
